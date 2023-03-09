fba5c7ffe9fec1abb19804d003fbff7e29243e65
Perry Hutchison <perry.hutchison@bluefruit.co.uk>
2023-03-09 18:14:57 +0000

    edited2

--- button2.py
+++ button2.py
@@ -10,21 +10,28 @@ button_pressed_count = 0
 button_last_state = False
 daemon_running = False
 
+def debounce(channel):
+    reading = GPIO.input(channel)
+    time.sleep(0.05)
+    reading = GPIO.input(channel)
+    return reading
+
 while True:
     # check for button press
     button_current_state = GPIO.input(4)
 
     if button_current_state != button_last_state:
         if button_current_state == False:
-            button_pressed_count += 1
-            print("Button pressed {} times".format(button_pressed_count))
+            if debounce(4) == GPIO.LOW:
+                button_pressed_count += 1
+                print("Button pressed {} times".format(button_pressed_count))
 
-            # if button pressed 3 times, run the CLI command
-            if button_pressed_count == 3:
-                print("Running edge-impulse-daemon command...")
-                subprocess.Popen(["edge-impulse-daemon"])
-                daemon_running = True
-                button_pressed_count = 0
+                # if button pressed 3 times, run the CLI command
+                if button_pressed_count == 3:
+                    print("Running edge-impulse-daemon command...")
+                    subprocess.Popen(["edge-impulse-daemon"])
+                    daemon_running = True
+                    button_pressed_count = 0
                 
         button_last_state = button_current_state
     else:
@@ -32,9 +39,10 @@ while True:
 
     # check for single button press
     if button_pressed_count == 1 and daemon_running:
-        print("Running SendData.py command...")
-        subprocess.call(["python3", "SendData.py"])
-        button_pressed_count = 0
+        if debounce(4) == GPIO.LOW:
+            print("Running SendData.py command...")
+            subprocess.call(["python3", "SendData.py"])
+            button_pressed_count = 0
 
     # add a small delay to avoid high CPU usage
     time.sleep(0.1)
