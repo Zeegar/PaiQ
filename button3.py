17518fe79ccdacf1fcb6f0f84fd4ac75124f3675
Perry Hutchison <perry.hutchison@bluefruit.co.uk>
2023-03-10 10:18:17 +0000

    edit 5

--- button3.py
+++ button3.py
@@ -23,28 +23,27 @@ while True:
 
     if button_current_state != button_last_state:
         if button_current_state == False:
-            if debounce(4) == GPIO.LOW:
-                button_pressed_count += 1
-                print("Button pressed {} times".format(button_pressed_count))
+            reading = debounce(4)
+            if reading == GPIO.LOW:
+                if not daemon_running:  # check if daemon is not already running
+                    button_pressed_count += 1
+                    print("Button pressed {} times".format(button_pressed_count))
 
-                # if button pressed 3 times in succession and daemon is not running, start the daemon and set the button count to 1
-                if button_pressed_count == 3 and not daemon_running:
-                    current_time = time.time()
-                    if current_time - button_last_time < 1:  # check if the 3 button presses were in succession
+                    # if button pressed 3 times, run the CLI command
+                    if button_pressed_count == 3:
                         print("Running edge-impulse-daemon command...")
                         subprocess.Popen(["edge-impulse-daemon"])
                         daemon_running = True
-                        button_pressed_count = 1
-                    button_last_time = current_time
-                else:
-                    button_pressed_count = 0  # reset button pressed count if conditions not met
+                        button_pressed_count = 1  # reset button pressed count to 1
                 
         button_last_state = button_current_state
-
+    else:
+        print("Button not pressed")
 
     # check for single button press
     if button_pressed_count == 1 and daemon_running:
-        if debounce(4) == GPIO.LOW:
+        reading = debounce(4)
+        if reading == GPIO.LOW:
             print("Running SendData.py command...")
             subprocess.call(["python3", "SendData.py"])
             daemon_running = False  # reset daemon_running flag
